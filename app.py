529e796dae8095cbdcad66e5816e6b850b66a3cb
5pponent <ahdwjdtprtm@gmail.com>
2023-09-23 19:12:37 +0900

    feat: Init base configuration

--- app.py
+++ app.py
@@ -2,7 +2,4 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {}
+from app_config import *
---
+++ app_config.py
@@ -0,0 +1,20 @@
+from app import app
+
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
+from fastapi.staticfiles import StaticFiles
+
+
+# Middlewares
+app.add_middleware(ProxyHeadersMiddleware, trusted_hosts=["*"])
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1"])
+
+
+# static path config
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# favicon config
+# @app.get("/favicon.ico", include_in_schema=False)
+# async def favicon():
+#     return FileResponse("static/favicon.ico")
