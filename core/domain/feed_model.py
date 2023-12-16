23c8e69410baebba6ac66e1b6701bb586ed7710b
Seong Heung Bae <mopil1102@gmail.com>
2023-12-16 17:36:43 +0900

    feat: add feed search

--- core/domain/feed_model.py
+++ core/domain/feed_model.py
@@ -1,17 +1,16 @@
 import peewee
-from playhouse.signals import Model, pre_save
 from playhouse.postgres_ext import BinaryJSONField
-from datetime import datetime
 
-from core.config.orm_config import db
-from core.config.var_config import KST, DB_SCHEMA
 from core.domain.base_entity import BaseEntity
 from core.domain.user_model import User
 
 
 class Feed(BaseEntity):
     user = peewee.ForeignKeyField(User, backref="user")
-    content = BinaryJSONField(default={})
+    title = peewee.CharField(max_length=100, null=False)
+    content = peewee.CharField(max_length=500, null=False)
+    images = peewee.CharField(null=True)  # separate by comma
+    tags = peewee.CharField(null=True)  # separate by comma
     view_count = peewee.IntegerField(default=0)
 
     class Meta:
