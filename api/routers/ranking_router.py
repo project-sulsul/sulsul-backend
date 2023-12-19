bed6db81f8edcc40b759a986258517741fd730af
5pponent <ahdwjdtprtm@gmail.com>
2023-12-19 17:30:49 +0900

    refactor: Refactor combination query function logic

--- api/routers/ranking_router.py
+++ api/routers/ranking_router.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends, Request
 
 from core.config.orm_config import get_db
 from api.config.middleware import auth_required
-from core.domain.combination_query_function import fetch_combination_ranking
+from core.domain import combination_query_function
 from core.dto.pairing_dto import PairingResponse
 from core.dto.combination_dto import CombinationResponse, CombinationListResponse
+
 
 router = APIRouter(
     prefix="/ranks",
@@ -19,48 +20,7 @@ router = APIRouter(
 )
 @auth_required
 async def get_combination_ranking(request: Request, order_by_popular: bool = True):
-    combinations = []
-    for record in fetch_combination_ranking(order_by_popular):
-        (
-            id,
-            count,
-            description,
-            alcohol_id,
-            alcohol_type,
-            alcohol_subtype,
-            alcohol_name,
-            alcohol_image,
-            alcohol_description,
-            food_id,
-            food_type,
-            food_subtype,
-            food_name,
-            food_image,
-            food_description,
-        ) = record
-        combinations.append(
-            CombinationResponse(
-                id=id,
-                alcohol=PairingResponse(
-                    id=alcohol_id,
-                    type=alcohol_type,
-                    subtype=alcohol_subtype,
-                    name=alcohol_name,
-                    image=alcohol_image,
-                    description=alcohol_description,
-                ),
-                food=PairingResponse(
-                    id=food_id,
-                    type=food_type,
-                    subtype=food_subtype,
-                    name=food_name,
-                    image=food_image,
-                    description=food_description,
-                ),
-                count=count,
-                description=description,
-            )
-        )
+    combinations = [CombinationResponse(**record) for record in combination_query_function.fetch_combination_ranking(order_by_popular)]
     return CombinationListResponse(combinations=combinations)
 
 
--- core/domain/combination_query_function.py
+++ core/domain/combination_query_function.py
@@ -2,8 +2,9 @@ from core.domain.pairing_model import Pairing
 from core.domain.combination_model import Combination
 
 
-def fetch_combination_ranking(order_by_popular: bool):
-    return (
+def fetch_combination_ranking(order_by_popular: bool) -> list:
+    result_list = list()
+    for record in (
         Combination.select(
             Combination.id,
             Combination.count,
@@ -31,4 +32,45 @@ def fetch_combination_ranking(order_by_popular: bool):
             on=(Combination.food == Pairing.alias("food_pairing").id),
         )
         .order_by(Combination.count.desc() if order_by_popular else Combination.count)
-    ).tuples()
+    ).tuples():
+        (
+            id,
+            count,
+            description,
+            alcohol_id,
+            alcohol_type,
+            alcohol_subtype,
+            alcohol_name,
+            alcohol_image,
+            alcohol_description,
+            food_id,
+            food_type,
+            food_subtype,
+            food_name,
+            food_image,
+            food_description,
+        ) = record
+        result_list.append(
+            {
+                "id": id,
+                "count": count,
+                "description": description,
+                "alcohol": {
+                    "id": alcohol_id,
+                    "type": alcohol_type,
+                    "subtype": alcohol_subtype,
+                    "name": alcohol_name,
+                    "image": alcohol_image,
+                    "description": alcohol_description
+                },
+                "food": {
+                    "id": food_id,
+                    "type": food_type,
+                    "subtype": food_subtype,
+                    "name": food_name,
+                    "image": food_image,
+                    "description": food_description
+                }
+            }
+        )
+    return result_list
