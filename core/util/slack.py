7f936734fa751548da823144a69bed49f038f84b
Seong Heung Bae <mopil1102@gmail.com>
2023-12-25 13:21:10 +0900

    fix: slack webhook url prod changed

--- core/util/slack.py
+++ core/util/slack.py
@@ -1,15 +1,23 @@
 import json
+import os
 
 import requests
 
-from core.config.secrets import SLACK_WEB_HOOK_URL
+from core.config.var_config import IS_PROD
+
+if IS_PROD:
+    webhook_rul = os.environ.get("SLACK_WEB_HOOK_URL")
+else:
+    from core.config import secrets
+
+    webhook_rul = secrets.SLACK_WEB_HOOK_URL
 
 
 async def send_slack_message(
     sender_name: str, channel: str, icon_emoji: str, message: str
 ):
     requests.post(
-        SLACK_WEB_HOOK_URL,
+        webhook_rul,
         data=json.dumps(
             {
                 "username": sender_name,
