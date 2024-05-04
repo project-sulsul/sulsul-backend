0e297dce841f1653f2b3e2d8d0aae13f61b9d1e5
5pponent <ahdwjdtprtm@gmail.com>
2024-05-04 17:25:16 +0900

    fix: Fix created_at field time mismatch

--- core/domain/base_entity.py
+++ core/domain/base_entity.py
@@ -1,11 +1,10 @@
-from typing import Dict
 from datetime import datetime
 
 import peewee
 
 from api.config.exceptions import NotFoundException
 from core.config.orm_config import db
-from core.config.var_config import KST, DB_SCHEMA
+from core.config.var_config import DB_SCHEMA
 
 
 class BaseEntity(peewee.Model):
@@ -15,15 +14,15 @@ class BaseEntity(peewee.Model):
 
     id = peewee.AutoField(primary_key=True)
     created_at = peewee.DateTimeField(
-        default=datetime.now(KST).strftime("%Y-%m-%d %H:%M:%S")
+        default=datetime.now,
     )
     updated_at = peewee.DateTimeField(
-        default=datetime.now(KST).strftime("%Y-%m-%d %H:%M:%S")
+        default=datetime.now,
     )
     is_deleted = peewee.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        self.updated_at = datetime.now(KST).strftime("%Y-%m-%d %H:%M:%S")
+        self.updated_at = datetime.now()
         return super().save(*args, **kwargs)
 
     def soft_delete(self):
@@ -40,11 +39,3 @@ class BaseEntity(peewee.Model):
         if entity is None or entity.is_deleted is True:
             raise NotFoundException(target_entity=cls, target_id=entity_id)
         return entity
-
-    @classmethod
-    def props(cls) -> Dict[str, any]:
-        return {
-            prop: typ
-            for prop, typ in dict(cls.__dict__).items()
-            if isinstance(typ, peewee.FieldAccessor)
-        }
