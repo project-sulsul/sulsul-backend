dad6af2cf3bb6c88440cde324624206f151dc66a
5pponent <ahdwjdtprtm@gmail.com>
2024-05-16 00:15:45 +0900

    test: Test batch

--- batch/ranking/lambda_function.py
+++ batch/ranking/lambda_function.py
@@ -1,101 +1,104 @@
-import os
-import sys
-sys.path.append(".")
-sys.path.append("/lambda")
-from pytz import timezone
-from datetime import datetime, timedelta
-from playhouse.pool import PooledPostgresqlExtDatabase
+try:
+    import os
+    import sys
+    sys.path.append(".")
+    sys.path.append("/lambda")
+    from pytz import timezone
+    from datetime import datetime, timedelta
+    from playhouse.pool import PooledPostgresqlExtDatabase
 
-from core.domain.ranking.ranking_model import Ranking
-from core.dto.pairing_dto import PairingResponse
-from core.domain.pairing.pairing_query_function import (
-    fetch_pairings_by_multiple_ids,
-)
-from core.domain.ranking.ranking_query_function import (
-    fetch_like_counts_group_by_alcohol,
-    fetch_like_counts_group_by_combination,
-)
-
-
-def lambda_handler(event, context):
-
-    today = datetime.now(tz=timezone("Asia/Seoul")).replace(hour=0, minute=0, second=0, microsecond=0)
-    start = today - timedelta(days=today.weekday() + 3)
-    end = start + timedelta(days=7)
-    print(start)
-    print(end)
-
-    db = PooledPostgresqlExtDatabase(
-        database="sulsul",
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
+    from core.domain.ranking.ranking_model import Ranking
+    from core.dto.pairing_dto import PairingResponse
+    from core.domain.pairing.pairing_query_function import (
+        fetch_pairings_by_multiple_ids,
     )
-    db.set_time_zone("Asia/Seoul")
-    db.connect()
-
-    # 술 랭킹
-    alcohol_ids = []
-    query = fetch_like_counts_group_by_alcohol(
-        start_date=start,
-        end_date=end,
-        execute=False,
+    from core.domain.ranking.ranking_query_function import (
+        fetch_like_counts_group_by_alcohol,
+        fetch_like_counts_group_by_combination,
     )
-    for row in query.execute():
-        alcohol_ids.append(row.alcohol_id)
 
-    alcohols_dict = {
-        pairing.id: pairing for pairing in fetch_pairings_by_multiple_ids(alcohol_ids)
-    }
 
-    alcohol_ranking = {}
-    for idx, alcohol_id in enumerate(alcohol_ids):
-        rank = idx + 1
-        alcohol_response = PairingResponse.from_orm(alcohols_dict[alcohol_id]).__dict__
-        alcohol_ranking[rank] = alcohol_response
+    def lambda_handler(event, context):
 
-    # 조합 랭킹
-    data = []
-    pairing_ids = set()
-    query = fetch_like_counts_group_by_combination(
-        start_date=start,
-        end_date=end,
-        execute=False,
-    )
-    for row in query.execute():
-        data.append(row.combined_ids)
-        pairing_ids.update(row.combined_ids)
+        today = datetime.now(tz=timezone("Asia/Seoul")).replace(hour=0, minute=0, second=0, microsecond=0)
+        start = today - timedelta(days=today.weekday() + 3)
+        end = start + timedelta(days=7)
+        print(start)
+        print(end)
 
-    pairings_dict = {
-        pairing.id: pairing
-        for pairing in fetch_pairings_by_multiple_ids(pairing_ids=pairing_ids)
-    }
+        db = PooledPostgresqlExtDatabase(
+            database="sulsul",
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+        db.set_time_zone("Asia/Seoul")
+        db.connect()
 
-    combination_ranking = {}
-    for idx, pairing_ids in enumerate(data):
-        rank = idx + 1
-        combination_ranking[rank] = []
-        for pairing_id in pairing_ids:
-            combination_ranking[rank].append(
-                PairingResponse.from_orm(pairings_dict[pairing_id]).__dict__
-            )
-    
-    import json
-    print("술 랭킹: ")
-    print(json.dumps(alcohol_ranking, ensure_ascii=False, indent=4))
-    print("조합 랭킹: ")
-    print(json.dumps(combination_ranking, ensure_ascii=False, indent=4))
+        # 술 랭킹
+        alcohol_ids = []
+        query = fetch_like_counts_group_by_alcohol(
+            start_date=start,
+            end_date=end,
+            execute=False,
+        )
+        for row in query.execute():
+            alcohol_ids.append(row.alcohol_id)
 
-    Ranking(
-        start_date=start,
-        end_date=end,
-        ranking={
-            "alcohol": alcohol_ranking,
-            "combination": combination_ranking,
-        },
-    ).save()
+        alcohols_dict = {
+            pairing.id: pairing for pairing in fetch_pairings_by_multiple_ids(alcohol_ids)
+        }
 
-    db.close()
+        alcohol_ranking = {}
+        for idx, alcohol_id in enumerate(alcohol_ids):
+            rank = idx + 1
+            alcohol_response = PairingResponse.from_orm(alcohols_dict[alcohol_id]).__dict__
+            alcohol_ranking[rank] = alcohol_response
 
-lambda_handler({}, {})
+        # 조합 랭킹
+        data = []
+        pairing_ids = set()
+        query = fetch_like_counts_group_by_combination(
+            start_date=start,
+            end_date=end,
+            execute=False,
+        )
+        for row in query.execute():
+            data.append(row.combined_ids)
+            pairing_ids.update(row.combined_ids)
+
+        pairings_dict = {
+            pairing.id: pairing
+            for pairing in fetch_pairings_by_multiple_ids(pairing_ids=pairing_ids)
+        }
+
+        combination_ranking = {}
+        for idx, pairing_ids in enumerate(data):
+            rank = idx + 1
+            combination_ranking[rank] = []
+            for pairing_id in pairing_ids:
+                combination_ranking[rank].append(
+                    PairingResponse.from_orm(pairings_dict[pairing_id]).__dict__
+                )
+        
+        import json
+        print("술 랭킹: ")
+        print(json.dumps(alcohol_ranking, ensure_ascii=False, indent=4))
+        print("조합 랭킹: ")
+        print(json.dumps(combination_ranking, ensure_ascii=False, indent=4))
+
+        Ranking(
+            start_date=start,
+            end_date=end,
+            ranking={
+                "alcohol": alcohol_ranking,
+                "combination": combination_ranking,
+            },
+        ).save()
+
+        db.close()
+
+except:
+    import traceback
+    print(traceback.format_exc())
